be7e9bb4122c3f45b767e96049ee11a6ce7aad0b
Shivansh-22866 <shivanshp0418@gmail.com>
2024-06-25 17:56:54 +0530

    CustomDialog component

--- src/app/[userId]/dashboard/page.tsx
+++ src/app/[userId]/dashboard/page.tsx
@@ -1,17 +1,28 @@
-import React from 'react'
+'use client'
+import React from 'react';
+import CustomDialog from '../../../components/CustomDialog';
 
 interface UserDetailsProps {
-    params: {
-      userId: string;
-    };
+  params: {
+    userId: string;
+  };
 }
 
 function Dashboard({ params }: UserDetailsProps) {
+  const dialogContent = {
+    title: 'Frover',
+    userName: 'User123',
+    email: 'abc@xyz.com',
+    buttons: ['Settings', 'Feedback', 'Help'],
+    userImage: '/logo.jpeg'
+  };
+
   return (
     <div className='ml-16'>
-        <h1>{params.userId}: Dashboard</h1>
+      <h1>{params.userId}: Dashboard</h1>
+      <CustomDialog content={dialogContent} />
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
