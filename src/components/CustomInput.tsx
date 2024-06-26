42a669cd6f906219a51b35ad26572ea2ddd3175a
Shivansh-22866 <shivanshp0418@gmail.com>
2024-06-26 18:19:08 +0530

    a

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -78,7 +78,7 @@ const CustomInput: React.FC<InputProps> = ({
         type={options.type}
         className={`mt-1 block w-full px-3 py-2 border ${
           error ? 'border-red-500' : 'border-gray-300'
-        } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-black`}
+        } rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm text-white`}
         placeholder={options.placeholder}
         value={options.value}
         onChange={handleChange}
